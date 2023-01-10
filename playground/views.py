906b9c2eb5577d09d3f25739925bbc140fc1d254
KMWaiing <kyf232001@gmail.com>
2023-01-10 22:52:44 +0800

    bug/fixed. Sending Emails.

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'service@domain.com', ['bob@domain.com'])
+        # send_mail('subject', 'message', '',['bob@domain.com'])
+        mail_admins('subject', 'messgae', html_message='messageeee')
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -181,3 +181,7 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '2525'
 DEFAULT_FROM_EMAIL = 'service@domain.com'
+
+ADMINS = [
+    ('Mosh', 'admin@domain.com')
+]
